6e12e2e26b7b5da6d0865a84f3ff6177f2001e39
neverwriter <plewczuk513@gmial.com>
2021-10-17 11:08:02 +0200

    Clean CustomerRegistrationService class

--- src/main/java/com/amigoscode/testing/customer/CustomerRegistrationService.java
+++ src/main/java/com/amigoscode/testing/customer/CustomerRegistrationService.java
@@ -15,7 +15,6 @@ public class CustomerRegistrationService {
     private final CustomerRepository customerRepository;
 
 
-
     @Autowired
     public CustomerRegistrationService(CustomerRegistrationRequest request, CustomerRepository customerRepository) {
         this.request = request;
@@ -26,18 +25,19 @@ public class CustomerRegistrationService {
 
     public void registerNewCustomer(@Valid @RequestBody CustomerRegistrationRequest request) {
 
-        Optional<Customer> customerOptional = customerRepository.selectCustomerByPhoneNumber(request.getCustomer().getPhoneNumber());
+        String phoneNumber = request.getCustomer().getPhoneNumber();
+
+        Optional<Customer> customerOptional = customerRepository
+                .selectCustomerByPhoneNumber(phoneNumber);
 
         if (customerOptional.isPresent()) {
-
-            if (customerOptional.get().getName().equals(request.getCustomer().getName())) {
+            if (customerOptional.get().getName()
+                    .equals(request.getCustomer().getName())) {
                 return;
-            } else {
-                throw new IllegalStateException("Phone number taken");
             }
-        } else {
-            customerRepository.save(request.getCustomer());
-            }
+            throw new IllegalStateException(String.format("Phone number [%s] is taken", phoneNumber));
+        }
+        customerRepository.save(request.getCustomer());
 
         // 1. PhoneNumber is taken
         // 2. if taken lets check if belong to same customer
